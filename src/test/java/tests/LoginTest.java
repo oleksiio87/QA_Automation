d6441b50e70a6dae71e73b679d69974d98630dd4
oleksiio87 <olexiio@gmail.com>
2016-01-23 14:30:39 +0200

    third commit

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -12,18 +12,18 @@ public class LoginTest {
 
     public static final String URL_SITE = "https://secure.filelocker.com";
 
-    public static final String LOGIN_FIELD = "#txtLogin";
-    public static final String LOGIN_BUTTON = "#loginBtnSecText";
+    public static final String CSS_LOGIN_FIELD = "#txtLogin";
+    public static final String CSS_LOGIN_BUTTON = "#loginBtnSecText";
     public static final String LOGIN_NAME = "minchekov160@hotmail.com";
 
-    public static final String PASSWORD_FIELD = "#txtPassword";
+    public static final String CSS_PASSWORD_FIELD = "#txtPassword";
     public static final String PASSWORD = "Qw1111";
 
-    public static final String INVALID_CREDENTIALS_MESSAGE = "#alertBox > div:nth-child(1) > center:nth-child(1)";
+    public static final String CSS_INVALID_CREDENTIALS_MESSAGE = "#alertBox > div:nth-child(1) > center:nth-child(1)";
     public static final String INVALID_PASSWORD = "test";
     public static final String INVALID_LOGIN = "test";
 
-    public static final String USERNAME_NAME = ".name > a:nth-child(1)";
+    public static final String CSS_USERNAME_NAME = ".name > a:nth-child(1)";
 
     public WebDriver driver;
 
@@ -36,20 +36,20 @@ public class LoginTest {
 
     @Test
     public void canLoginWithValidCredentials() {
-        driver.findElement(By.cssSelector(LOGIN_FIELD)).sendKeys(LOGIN_NAME);
-        driver.findElement(By.cssSelector(PASSWORD_FIELD)).sendKeys(PASSWORD);
-        driver.findElement(By.cssSelector(LOGIN_BUTTON)).click();
-        Assert.assertEquals(driver.findElement(By.cssSelector(USERNAME_NAME)).getText(), "Test Pass");
+        driver.findElement(By.cssSelector(CSS_LOGIN_FIELD)).sendKeys(LOGIN_NAME);
+        driver.findElement(By.cssSelector(CSS_PASSWORD_FIELD)).sendKeys(PASSWORD);
+        driver.findElement(By.cssSelector(CSS_LOGIN_BUTTON)).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector(CSS_USERNAME_NAME)).getText(), "Test Pass");
     }
 
     @Test
     public void cannotLoginWithInvalidCredentials() {
         driver.manage().deleteAllCookies();
         driver.navigate().refresh();
-        driver.findElement(By.cssSelector(LOGIN_FIELD)).sendKeys(INVALID_LOGIN);
-        driver.findElement(By.cssSelector(PASSWORD_FIELD)).sendKeys(INVALID_PASSWORD);
-        driver.findElement(By.cssSelector(LOGIN_BUTTON)).click();
-        Assert.assertEquals(driver.findElement(By.cssSelector(INVALID_CREDENTIALS_MESSAGE)).getText(), "Unable to verify credentials");
+        driver.findElement(By.cssSelector(CSS_LOGIN_FIELD)).sendKeys(INVALID_LOGIN);
+        driver.findElement(By.cssSelector(CSS_PASSWORD_FIELD)).sendKeys(INVALID_PASSWORD);
+        driver.findElement(By.cssSelector(CSS_LOGIN_BUTTON)).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector(CSS_INVALID_CREDENTIALS_MESSAGE)).getText(), "Unable to verify credentials");
     }
 
     @AfterClass
